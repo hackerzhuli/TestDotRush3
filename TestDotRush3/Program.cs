064ccd8b6d050d2197befd72e38d2c95951947af
朱力 <790403624@qq.com>
2025-07-07 17:36:08 +0800

    docs: fix typos and improve XML documentation comments
    Correct "Similiar" to "Similar" in multiple XML documentation comments
    Fix reference to Console.WriteLine method with proper parameter signature

--- TestDotRush3/Program.cs
+++ TestDotRush3/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     /// <summary>
     /// Adds two numbers <br/>
-    /// Similiar to <see cref="Add3"/> but only add 2 numbers <br/>
+    /// Similar to <see cref="Add3"/> but only add 2 numbers <br/>
     /// </summary>
-    /// <param name="a">The first number, see <see cref="Console.WriteLine"/> from <see cref="Console"/></param>
+    /// <param name="a">The first number, see <see cref="Console.WriteLine(string)"/> from <see cref="Console"/></param>
     /// <param name="b">The second number</param>
     /// <returns>The sum of the two numbers</returns>
     /// <remarks>
     /// This method adds two numbers and returns the sum. <br/>
     /// Some other important note: <br/>
-    /// This method is similiar to <see cref="Add3"/> but only add 2 numbers.
+    /// This method is similar to <see cref="Add3"/> but only add 2 numbers.
     /// </remarks>
     private static int Add(int a, int b){
         return a + b;
@@ -20,7 +20,7 @@ class Program
 
     /// <summary>
     /// Adds three numbers <br />
-    /// Similiar to <see cref="Add(int, int)"/> but only add 3 numbers <br/>
+    /// Similar to <see cref="Add(int, int)"/> but only add 3 numbers <br/>
     /// </summary>
     private static int Add(int a, int b, int c){
         return a + b + c;
@@ -28,7 +28,7 @@ class Program
 
     /// <summary>
     /// Adds two numbers <br />
-    /// Similiar to <see cref="Add3"/> but only add 2 numbers <br/>
+    /// Similar to <see cref="Add3"/> but only add 2 numbers <br/>
     /// </summary>
     /// <param name="a">The first number</param>
     /// <param name="b">The second number</param>
